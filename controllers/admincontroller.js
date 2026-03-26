d4c5d7e1b29807b76ad1b5d00238438d30cfab16
Rinku yadav <rinkuyadav2001g@email.com>
2026-03-26 12:41:05 +0530

    update file

--- controllers/admincontroller.js
+++ controllers/admincontroller.js
@@ -1,5 +1,5 @@
 const Order = require("../models/Order");
-const Product = require("../models/Product");
+const Product = require("../models/product");
 
 /* ================= HELPER: CALCULATE STATS ================= */
 
